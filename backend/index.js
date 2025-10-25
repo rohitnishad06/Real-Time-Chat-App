c439265af486538820cc80d4a257e6599813679d
rohit-06-nishad <rohitnishad123467@gmail.com>
2025-10-25 21:45:56 +0530

    url updated

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://real-time-chat-app-1-gl30.onrender.com',
     credentials:true
 };
 app.use(cors(corsOption)); 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="http://localhost:8080"
+export const BASE_URL="https://real-time-chat-app-1-gl30.onrender.com"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
